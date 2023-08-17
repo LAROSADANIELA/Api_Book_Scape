571ab1200704b99cb46ca1b720b7e50e061a7451
NadirMansur <nadirmansur89@gmail.com>
2023-08-16 23:20:10 -0300

    Se realizó la busqueda por Author y Title en el controller allbooks.js 	modified:   src/controllers/allbooks.js

--- src/controllers/allbooks.js
+++ src/controllers/allbooks.js
@@ -1,68 +1,97 @@
-const {Book, Language, Author} = require("../db")
-const {Op} = require('sequelize')
-
-
+const { Book, Language, Author, Publisher } = require("../db");
+const { Op } = require("sequelize");
 
 //titulo imagen rating author precio CARD
 const allBooks = async (req, res, next) => {
-    try {
-        const {target} = req.query
-        if(!target){
-            const allBooksDB = await Book.findAll({
-                attributes: ['title','price','rating_ave','image'],
-                include: {
-                    model: Author,
-                    attributes: ["name"],
-                    through: {
-                      attributes: [],
-                    },
-                  },
-            })
-            return res.send(allBooksDB)
-        }
-        console.log("Busqueda Por Book")
-        console.log("Buscando",target)
-        const allBooksDB = await Book.findAll({
-            where:{[Op.or]:[
-                {title: {[Op.iLike]:`%${target}%`}},
-            /*{'$author_book.AuthorId$': {[Op.iLike]:`%${target}%`}},*/]}
-                },
-                // include: {
-                //     model: Author,
-                //     attributes: ["name"],
-                //     through: {
-                //       attributes: [],
-                //     },
-                //   },
-            
-            {
-            attributes: ['title','price','rating_ave','image'],
-            include: {
-                model: Author,
-                attributes: ["name"],
-                through: {
-                  attributes: [],
-                },
-            },
-        })
-        console.log("resultado de busqueda por book",allBooksDB)
-        if(allBooksDB.length === 0){
-            console.log("Busqueda Por Author")
-            console.log("Buscando",target)
-           const author =  await Author.findAll({
-                where: {
-                    name: {
-                        [Op.iLike]:`%${target}%`
-                    }
-                },
-                include: Book,
-            });
-           return res.send(author)
-        }
-         res.send(allBooksDB)
-    } catch (error) {
-        next(error)
+  try {
+    const { target } = req.query;
+    if (!target) {
+      const allBooksDB = await Book.findAll({
+        attributes: ["title", "price", "rating_ave", "image"],
+        include: {
+          model: Author,
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
+        },
+      });
+      return res.send(allBooksDB);
     }
-}
+    console.log("Busqueda Por Book");
+    console.log("Buscando", target);
+    const allBooksDB = await Book.findAll({
+      attributes: [
+        "id_book",
+        "isbn",
+        "title",
+        "published_date",
+        "price",
+        "description",
+        "rating_ave",
+        "image",
+        "page_count",
+        "url",
+      ],
+      where: {
+        [Op.or]: [
+          { title: { [Op.iLike]: `%${target}%` } },
+          /*{'$author_book.AuthorId$': {[Op.iLike]:`%${target}%`}},*/
+        ],
+      },
+      include: [
+        {
+          model: Author,
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
+        },
+        {
+          model: Language,
+          attributes: ["language"],
+        },
+        {
+          model: Publisher,
+          attributes: ["name"],
+        },
+      ],
+    });
+    console.log("resultado de busqueda por book", allBooksDB);
+    if (allBooksDB.length === 0) {
+      console.log("Busqueda Por Author");
+      console.log("Buscando", target);
+      const author = await Author.findAll({
+        attributes: ["name"],
+        where: {
+          name: {
+            [Op.iLike]: `%${target}%`,
+          },
+        },
+        include: [
+          {
+            model: Book,
+            attributes: [
+              "id_book",
+              "isbn",
+              "title",
+              "published_date",
+              "price",
+              "description",
+              "rating_ave",
+              "image",
+              "page_count",
+              "url",
+            ],
+          },
+        ],
+      });
+      return res.send(author);
+    }
+    res.send(allBooksDB);
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports= allBooks
+module.exports = allBooks;
