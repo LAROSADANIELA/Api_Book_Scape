763c4e3b23ba542ee34f67f19d75a4136b7235bc
FernandoAdrogue <chinoadrogue@gmail.com>
2023-08-17 19:14:20 -0300

    se agrego busqueda por publisher

--- src/controllers/allbooks.js
+++ src/controllers/allbooks.js
@@ -95,8 +95,9 @@
 // };
 
 // module.exports = allBooks;
-const { Book, Language, Author } = require("../db");
+const { Book, Publisher , Author , Language } = require("../db");
 const { Op } = require("sequelize");
+const publisher = require("../models/publisher");
 
 //titulo imagen rating author precio CARD
 const allBooks = async (req, res, next) => {
@@ -117,13 +118,23 @@ const allBooks = async (req, res, next) => {
           "page_count",
           "url",
         ],
-        include: {
-          model: Author,
-          attributes: ["name"],
-          through: {
-            attributes: [],
+        include: [
+          {
+            model: Author,
+            attributes: ["name"],
+            through: {
+              attributes: [],
+            },
           },
-        },
+          {
+            model: Publisher,
+            attributes: ["name"],
+          },
+          {
+            model: Language,
+            attributes: ["language"],
+          },
+      ],
       });
       return res.send(allBooksDB);
     }
@@ -144,13 +155,23 @@ const allBooks = async (req, res, next) => {
         "page_count",
         "url",
       ],
-      include: {
-        model: Author,
-        attributes: ["name"],
-        through: {
-          attributes: [],
+      include:[
+        {
+          model: Author,
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
         },
-      },
+        {
+          model: Publisher,
+          attributes: ["name"],
+        },
+        {
+          model: Language,
+          attributes: ["language"],
+        },
+    ],
     });
     const authorBooks = await Book.findAll({
       attributes: [
@@ -165,18 +186,62 @@ const allBooks = async (req, res, next) => {
         "page_count",
         "url",
       ],
-      include: {
-        model: Author,
-        where: {
-          name: { [Op.iLike]: `%${target}%` },
+      include: [
+        {
+          model: Author,
+          where: {
+            name: { [Op.iLike]: `%${target}%` },
+          },
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
         },
-        attributes: ["name"],
-        through: {
-          attributes: [],
+        {
+          model: Publisher,
+          attributes: ["name"],
         },
-      },
+        {
+          model: Language,
+          attributes: ["language"],
+        },
+    ],
     });
-    const targetBooks = [...titletBooks, ...authorBooks];
+    const publisherBooks = await Book.findAll({
+      attributes: [
+        "id_book",
+        "isbn",
+        "title",
+        "published_date",
+        "price",
+        "description",
+        "rating_ave",
+        "image",
+        "page_count",
+        "url",
+      ],
+      include:[
+        {
+          model: Author,
+          attributes: ["name"],
+          through: {
+            attributes: [],
+          },
+        },
+        {
+          model: Publisher,
+          where: {
+            name: { [Op.iLike]: `%${target}%` },
+          },
+          attributes: ["name"],
+        },
+        {
+          model: Language,
+          attributes: ["language"],
+        },
+      ],
+    });
+    const targetBooks = [...titletBooks, ...authorBooks, ...publisherBooks];
     if (targetBooks.length === 0)
       return res.status(404).json({ error: "Not Found" });
     res.send(targetBooks);
