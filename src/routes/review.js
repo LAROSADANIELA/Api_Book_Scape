52d8425852cca1ec9f0f6463b3db7fe4c73d1a56
NadirMansur <nadirmansur89@gmail.com>
2023-08-20 16:04:39 -0300

    Se creó controller getUserReviews y su respectiva ruta, ambos cambios en funcionamiento 	new file:   src/controllers/getUserReviews.js         modified:   src/routes/review.js

--- src/routes/review.js
+++ src/routes/review.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const createReview = require('../controllers/createReview');
+const userReviews = require('../controllers/getUserReviews');
 
 router.post('/create', createReview);
+router.get('/:id', userReviews);
 
 module.exports = router;
