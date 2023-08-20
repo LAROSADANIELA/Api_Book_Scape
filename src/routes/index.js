bcd126879f309f7dad3dd33bb9d2736c069adebb
NadirMansur <nadirmansur89@gmail.com>
2023-08-20 14:13:14 -0300

    se creo ruta reviews en src/routes/index.js y se creo archivo de rutas review.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ var express = require("express");
 const books = require("./books");
 const users = require("./users");
 const favorite = require("./favorite");
+const review = require("./review");
 
 var router = express.Router();
 
 router.use("/books", books);
 router.use("/users", users);
 router.use("/favorites", favorite);
+router.use("/reviews", review);
 
 module.exports = router;
