0be0438b8aa7047328931d6fa0416f34db3f924c
LA ROSA DANELA <danielaalarosa@gmail.com>
2023-09-02 21:39:14 -0300

    se creo controlador para enviar notificaciones por correo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const books = require('./books')
-const users = require('./users')
-const favorite= require('./favorite')
-const review = require('./review')
-const shoppingcart = require('./r.shoppingcart')
+const books = require("./books");
+const users = require("./users");
+const favorite = require("./favorite");
+const review = require("./review");
+const shoppingcart = require("./r.shoppingcart");
+const mail = require("./mail");
 
 const router = express.Router();
 
-router.use('/books', books);
-router.use('/users', users);
+router.use("/books", books);
+router.use("/users", users);
 router.use("/favorites", favorite);
 router.use("/reviews", review);
 router.use("/shoppingcart", shoppingcart);
-
+router.use("/mail", mail);
 
 module.exports = router;
