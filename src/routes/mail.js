2ed429105dc2cdaadcdbc61111557102d19c04aa
zimlama <zimlama@gmail.com>
2023-09-05 14:01:18 -0500

    delete all

--- src/routes/mail.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userCreated = require("../controllers/mail");
-
-router.put("/signup", userCreated);
-module.exports = router;
